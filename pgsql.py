9e60048702c7d48b34d004f2e1be4a6fff00b97e
Marcos Tolosa <marcostolosa@protonmail.com>
2019-04-13 16:26:23 -0300

    refactor pgsql

--- pgsql.py
+++ pgsql.py
@@ -7,13 +7,14 @@ from configparser import ConfigParser
 
 db_id, db_cnpj, db_name, db_email = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
 
+sql = """ INSERT INTO companies ( id, cnpj, name, email )
+          VALUES ( %s, %s, '%s', '%s' )
+      """ % (db_id, db_cnpj, db_name, db_email)
+
 def config(filename='db.ini', section='postgresql'):
-    # create a parser
     parser = ConfigParser()
-    # read config file
     parser.read(filename)
 
-    # get section, default to postgresql
     db = {}
     if parser.has_section(section):
         params = parser.items(section)
@@ -26,39 +27,23 @@ def config(filename='db.ini', section='postgresql'):
 
 
 def connect(db_id, db_cnpj, db_name, db_email):
-    """ Connect to the PostgreSQL database server """
     conn = None
     try:
-        # read connection parameters
         params = config()
-
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
         conn = psycopg2.connect(**params)
 
-        # create a cursor
         cur = conn.cursor()
-
- # execute a statement
-        print('Inserting data on PostgreSQL...')
-        sql = """ INSERT INTO companies ( id, cnpj, name, email )
-                  VALUES ( %s, %s, '%s', '%s' )
-            """ % (db_id, db_cnpj, db_name, db_email)
-
         cur.execute(sql)
+
         conn.commit()
         count = cur.rowcount
         print(count, "Record inserted successfully into mobile table")
-
-     # close the communication with the PostgreSQL
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
         if conn is not None:
-            cur.close()
             conn.close()
-            print('Database connection closed.')
 
 
 if __name__ == '__main__':
